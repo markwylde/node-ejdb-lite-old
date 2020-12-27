bd445ef5909a6ff91d8d4fec6bb3970fe9e82f5d
Mark <me@markwylde.com>
2020-12-27 19:49:03 +1000

    Apply latest updates from softmotions

---
+++ applyUpdatesFromSoftmotions.sh
@@ -0,0 +1,10 @@
+rm -rf .vscode \
+  docker \
+  installer \
+  man \
+  src/bindings/ejdb2_node/yarn.lock \
+  src/bindings/ejdb2_android \
+  src/bindings/ejdb2_dart \
+  src/bindings/ejdb2_flutter \
+  src/bindings/ejdb2_jni \
+  src/bindings/ejdb2_react_native
