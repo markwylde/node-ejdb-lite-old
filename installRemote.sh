6e3dc50db37b0ff3d181655186e6ef442e2bede8
Mark <me@markwylde.com>
2021-06-10 21:14:55 +1000

    Another attempt

--- installRemote.sh
+++ installRemote.sh
@@ -3,8 +3,13 @@ version=$(node -p "require('./package.json').version")
 (. /etc/os-release) || true
 if [ `which apk` ]; then
   os=alpine
+  nodedir=build/src/bindings/ejdb2_node/ejdb2_node/linux-x64
 elif [ "$(uname)" = "Linux" ]; then
   os=linux
+  nodedir=build/src/bindings/ejdb2_node/ejdb2_node/linux-x64
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  os=darwin
+  nodedir=build/src/bindings/ejdb2_node/ejdb2_node/darwin-x64
 else
   echo "Could not detect operating system"
   uname
@@ -19,5 +24,5 @@ echo "Your OS is $os"
 
 downloadUrl="https://github.com/markwylde/node-ejdb-lite/releases/download/v${version}/ejdb2_node_x64_${os}_14.x.node"
 echo "Downloading $downloadUrl"
-mkdir -p build/src/bindings/ejdb2_node/ejdb2_node/linux-x64
-wget -O build/src/bindings/ejdb2_node/ejdb2_node/linux-x64/ejdb2_node.node $downloadUrl
+mkdir -p $nodedir
+wget -O $nodedir/ejdb2_node.node $downloadUrl
