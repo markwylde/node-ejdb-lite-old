d28187159c51f398d50e9ef92c8785259b3d7590
Mark Wylde <markwylde@users.noreply.github.com>
2021-06-10 21:56:54 +1000

    Add macos build (#20)
    * Switch to curl for macos

--- installRemote.sh
+++ installRemote.sh
@@ -25,4 +25,4 @@ echo "Your OS is $os"
 downloadUrl="https://github.com/markwylde/node-ejdb-lite/releases/download/v${version}/ejdb2_node_x64_${os}_14.x.node"
 echo "Downloading $downloadUrl"
 mkdir -p $nodedir
-wget -O $nodedir/ejdb2_node.node $downloadUrl
+curl -L -o $nodedir/ejdb2_node.node $downloadUrl
