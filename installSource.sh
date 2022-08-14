b311f0eb1344fa43d263186de48f890a34d46024
Mark Wylde <me@markwylde.com>
2022-08-14 09:37:50 +0100

    Attempt to force c 11

--- installSource.sh
+++ installSource.sh
@@ -1,6 +1,10 @@
 rm -rf ejdb
 git clone --recursive https://github.com/Softmotions/ejdb.git
 cd ejdb
+
+sed -i '1s/^/set (CMAKE_C_STANDARD 11)\n/' src/CMakeLists.txt
+
+
 mkdir ./build
 cd build
 cmake .. -DBUILD_NODEJS_BINDING=ON -DCMAKE_BUILD_TYPE=Release
