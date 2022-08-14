1308dd6a43c024651bd8112277680749f0a0af13
Mark Wylde <me@markwylde.com>
2022-08-14 11:37:38 +0100

    Fix

--- installSource.sh
+++ installSource.sh
@@ -6,4 +6,4 @@ cd build
 cmake .. -DBUILD_NODEJS_BINDING=ON -DCMAKE_BUILD_TYPE=Release
 make
 cd ../../
-cp ./ejdb/build/src/bindings/ejdb2_node/ejdb2_node/linux-x64/ejdb2_node.node ./src/ejdb2_node.node
+cp ./ejdb/build/src/bindings/ejdb2_node/ejdb2_node/*/ejdb2_node.node ./src/ejdb2_node.node
