fa7239a1bee8c48bff2975baa8f0a079ddaec22f
Mark Wylde <me@markwylde.com>
2022-08-14 10:39:09 +0100

    Arghh

--- installSource.sh
+++ installSource.sh
@@ -3,7 +3,7 @@ git clone --recursive https://github.com/Softmotions/ejdb.git
 cd ejdb
 mkdir ./build
 cd build
-CC=gcc-11 cmake .. -DBUILD_NODEJS_BINDING=ON -DCMAKE_BUILD_TYPE=Release
+CC=gcc-11 CXX=g++-11 cmake .. -DBUILD_NODEJS_BINDING=ON -DCMAKE_BUILD_TYPE=Release
 make
 cd ../../
 cp ./ejdb/build/src/bindings/ejdb2_node/ejdb2_node/linux-x64/ejdb2_node.node ./src/ejdb2_node.node
